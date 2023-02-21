01c8d27842aa4cfa2f9c3ee70c4bc3ed4f4a2239
Sebastian009w <ia3266505@gmail.com>
2023-02-21 09:03:58 -0500

    adelanto del drag and drop

--- src/components/Add.tsx
+++ src/components/Add.tsx
@@ -1,5 +1,5 @@
 import { PLAY } from "../interfaces/play";
-import { Box } from "../components/Box";
+import Board from "./Board";
 import { DndProvider } from "react-dnd";
 import { HTML5Backend } from "react-dnd-html5-backend";
 
@@ -11,7 +11,7 @@ export const PlayAdd = ({ title }: PLAY) => {
           <h2 className="text-2xl text-center p-3">{title}</h2>
         </div>
         <div className="flex justify-around h-screen items-center">
-          <Box id={1}/>
+          <Board />
         </div>
       </DndProvider>
     </>
